0a0edb3abe14baf2c7f2f5132a4605f45de948ac
White Nguyen <whitenguyen0909@gmail.com>
2023-12-04 20:58:21 -0700

    Add project files.

---
+++ AoC/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Given an array of strings, each string contains 2 numbers, put two numbers together to become a 2-digit number.
+// Find the sum of all those numbers.
+
+
+/*
+ Create a new array of integer.
+ Loop through for the first array, l
+ */
