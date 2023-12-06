3f41005d814cbd4017eecb64529201ba674d41cb
White Nguyen <whitenguyen0909@gmail.com>
2023-12-05 23:05:54 -0700

    day one

---
+++ AoC/2023/DayOne.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+/*Trebuchet*/
+public class DayOne
+{
+    //part 1
+    public static int GetSum(int sum, int input)
+    {
+        return sum + input;
+    }
+
+    //part 2
+    public int Solve (string[] lines, string regex)
+    {
+        //Obtain the data source or sources | lines
+        //Create the query
+        //Execute the query
+        return (
+            from string line in lines 
+            let first = Regex.Match(line, regex)
+            let last = Regex.Match(line, regex, RegexOptions.RightToLeft)
+            select ParseMatch(first.Value) * 10 + ParseMatch(last.Value)
+            ).Sum();
+    }
+
+    private static int ParseMatch(string match)
+    {
+        return match switch
+        {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            string digit => int.Parse(digit)
+        };
+    }
+}
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,10 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode;
+
 // Given an array of strings, each string contains 2 numbers, put two numbers together to become a 2-digit number.
 // Find the sum of all those numbers.
-
-
 /*
- Create a new array of integer.
- Loop through for the first array, l
+ Create a new ~~array~~ List of integer.
+ Loop through for the first array, loop through each string to put together a 2-digit-number
+ Sum them up from the new array
  */
+
+string path = "input.txt";
+string[] lines = File.ReadAllLines(path);
+int sumPartOne = 0;
+
+//part 1
+foreach (string line in lines)
+{
+    string numbersAsString = "";
+    foreach (char ch in line)
+    {
+        if (char.IsDigit(ch))
+        {
+            numbersAsString += ch;
+        }
+    }
+    string pair = string.Concat(numbersAsString[0], numbersAsString[numbersAsString.Length - 1]);
+    sumPartOne = DayOne.GetSum(sumPartOne, Int32.Parse(pair));
+};
+Console.WriteLine(sumPartOne);
+
+DayOne dayOne = new DayOne();
+//part 2
+int sumPartTwo = 0;
+string regex = "\\d|one|two|three|four|five|six|seven|eight|nine";
+sumPartTwo = dayOne.Solve(lines, regex);
+Console.WriteLine(sumPartTwo);
